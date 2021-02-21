f912e066771d984640751afad9f9f3100d30ce1b
Sébastien Mascha <smascha@juniorisep.com>
2021-02-21 21:10:11 +0100

    :lipstick: Improve Toc generation

--- frontend/apps/how_it_works.py
+++ frontend/apps/how_it_works.py
@@ -2,11 +2,45 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-def app():
+from .toc import Toc
+
+
+def toc_content(toc: Toc):
     st.title('How it works?')
-    st.markdown("### Context")
-    st.markdown("### Research questions")
-    st.markdown("### Data Science Pipeline - Dataiku")
-    st.markdown("### Analysis")
-    st.markdown("### Our Model")
-    st.markdown("### Deployment")
+    st.markdown("< PHOTO >")
+    toc.placeholder()
+
+    # ----- HR Analytics: Job Change of Data Scientists -----
+    toc.title("HR Analytics")
+    toc.header("Abstract")
+    toc.header("Research Questions")
+
+    # ----- Data Science Pipeline -----
+    toc.title("Data Science Pipeline")
+    toc.header("Workspace: Dataiku & Colab")
+    toc.header("Methods & Results")
+
+    # ----- Deployment in Production -----
+    toc.title("Deployment in Production")
+    toc.header("Docker: FastAPI + Streamlit")
+    toc.header("Load Balancer: Traefik")
+    toc.header("Kubernetes")
+    toc.header("Data Persistence - Microsoft SQL")
+    st.markdown("""
+        We decided to keep this website as stateless for privacy reason.\n\
+        Nevertheless, we planned and tested the implementation of Microsoft SQL.
+        """)
+
+    # ----- Conclusion -----
+    toc.title("Deployment in Production")
+
+    # ----- References -----
+    toc.title("References")
+
+
+def app():
+
+    toc = Toc()
+    st.sidebar.markdown("# Table of Contents")
+    toc_content(toc)
+    toc.generate()
