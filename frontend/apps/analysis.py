573f130835a99e0cf4d55d570cad0217883bb8ba
unknown <thomaslecouedic@hotmail.fr>
2021-02-25 16:48:48 +0100

    modified reasearch analysis

--- frontend/apps/analysis.py
+++ frontend/apps/analysis.py
@@ -10,13 +10,17 @@ PATH_RES_FOLDER = os.getcwd() + "/res/"
 def app():
     st.title("Research & Analysis")
 
+    st.markdown("""
+    **You can find more relevant descriptive analysis in the "Descriptive Analysis" part of the notebook, in the [GitHub Project](https://github.com/sebastienmascha/data-science-viz-streamlit-hr-analytics/blob/main/Python3_6_Notebook.ipynb).**
+    """)
+
     st.image(PATH_RES_FOLDER + "img/barplot_experience_companytype.png")
     st.markdown("This barplot shows us, considering our study, that startup employees have less experience than employees from other company types. We can deduce from this graph that it is rather people who are starting or just beginning their professional career who are entering the world of startup and innovation.")
 
     st.image(PATH_RES_FOLDER + "img/barplot_lastnewjob_enrolleduniversity.png")
     st.markdown("This barplot shows us, considering our study, that the people without any enrollment took on average more time to find their actual job then those who are following courses.")
 
-    st.image(PATH_RES_FOLDER + "img/boxplot_city_dev_index_experience.png")
+    st.image(PATH_RES_FOLDER + "img/boxplot_experiencecategorical_citydevelopmentindex.png")
     st.markdown("This boxplot allows us to highlight that the people with a lot of experience are living essentially in developed cities whereas for Junior and Confirmed the distribution is more dispersed and on average they live in slightly less developed cities than Senior.")
 
     st.image(PATH_RES_FOLDER + "img/boxplot_educationlevel_lastnewjob.png")
@@ -25,9 +29,6 @@ def app():
     st.image(PATH_RES_FOLDER + "img/histo_experience_groupby_enrolleduniversity.png")
     st.markdown("We can see on that histogram, that the more work experience people have, the less they are enrolled in full time or part time course.")
 
-    st.markdown("""
-    **You can find more relevant descriptive analysis in the "Descriptive Analysis" part of the notebook, [here](https://github.com/sebastienmascha/data-science-viz-streamlit-hr-analytics/blob/main/Python3_6_Notebook.ipynb).**
-    """)
 
 
 
